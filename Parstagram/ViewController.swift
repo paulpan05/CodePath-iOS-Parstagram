dbde51b5863e11b5827a94d473b68d0cab1b43b9
Paul Pan <panjunhong05@gmail.com>
2020-02-26 20:22:24 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Paul Pan on 20/2/26.
+//  Copyright © 2020 Paul Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
